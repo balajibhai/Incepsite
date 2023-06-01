95c3f239e3486a4ed8a00375bdf43ef07c6e5872
Balaji Narasimhan <55449960+balajibhai@users.noreply.github.com>
2023-06-01 15:07:23 +0530

    hamburger icon kept with responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,71 @@
 import Image from "./Image";
 import { ThemeButton } from "./ThemeButton";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ColorModeContext } from "./Theme";
 import { logo, logoTitle } from "../Constants";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const Navigate = styled.div`
+  display: flex;
+  width: 83%;
+  align-items: center;
+  gap: 15px;
+  font-family: Diatype, Arial, sans-serif;
+  color: ${(props) => props.theme};
+
+  @media (max-width: 1321px) {
+    display: none;
+  }
+`;
+
+const Hamburger = styled.div`
+  display: none;
+
+  @media (max-width: 1321px) {
+    display: block;
+    cursor: pointer;
+  }
+`;
+
+const Parent = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 43px;
+  width: 100%;
+  gap: 270px;
+  padding-left: 5%;
+
+  @media (max-width: 1321px) {
+    display: flex;
+    align-items: center;
+    margin: 43px;
+    width: unset;
+    gap: 0px;
+    padding-left: 5%;
+    justify-content: space-between;
+  }
+`;
+
+const Content = styled.div`
+  display: flex;
+  width: 60%;
+
+  @media (max-width: 1321px) {
+    width: unset;
+  }
+`;
 
 const Header = () => {
   const { mode } = useContext(ColorModeContext);
   const theme = mode === "dark" ? "white" : "black";
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   const navigate = [
     "ABOUT",
     "DELEGATIO PROGRAM",
@@ -24,14 +83,6 @@ const Header = () => {
       fontFamily: "Diatype,Arial,sans-serif",
       color: theme,
     },
-    parent: {
-      display: "flex",
-      alignItems: "center",
-      margin: "43px",
-      width: "100%",
-      gap: "270px",
-      paddingLeft: "5%",
-    },
     image: {
       display: "flex",
       gap: "11px",
@@ -41,7 +92,7 @@ const Header = () => {
     },
   };
   return (
-    <div style={classes.parent}>
+    <Parent>
       <div style={classes.image}>
         <Image
           src={logo}
@@ -54,15 +105,18 @@ const Header = () => {
           variation={{ width: "145px", height: "28px" }}
         />
       </div>
-      <div style={{ display: "flex", width: "60%" }}>
-        <div style={classes.navigate}>
+      <Content>
+        <Navigate theme={theme}>
           {navigate.map((item) => {
             return <div style={{ cursor: "pointer" }}>{item}</div>;
           })}
-        </div>
-        <ThemeButton />
-      </div>
-    </div>
+          <ThemeButton />
+        </Navigate>
+        <Hamburger onClick={toggleDropdown}>
+          <FontAwesomeIcon icon={faBars} />
+        </Hamburger>
+      </Content>
+    </Parent>
   );
 };
 
